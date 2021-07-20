631039d32e24502b166981d1729fd021b9c171b7
milan <milan@example.com>
2021-07-20 16:30:26 +0200

    first commit

---
+++ app/src/main/java/rsphere4/com/androidlibraryexercise/MainActivity.kt
@@ -0,0 +1,16 @@
+package rsphere4.com.androidlibraryexercise
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        
+
+
+    }
+}
---
+++ mylibrary/src/main/java/rsphere4/com/mylibrary/ToastMessage.kt
@@ -0,0 +1,16 @@
+package rsphere4.com.mylibrary
+
+import android.content.Context
+import android.widget.Toast
+
+
+
+
+class ToastMessage {
+
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+
+
+}
